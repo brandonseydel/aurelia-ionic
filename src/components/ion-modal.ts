4f052285f59c98293e7ffce8b2d9414603abf8a7
Brandon Seydel <brandonseydel@hotmail.com>
2023-10-24 10:46:17 -0500

    try export of type

--- src/components/ion-modal.ts
+++ src/components/ion-modal.ts
@@ -1,7 +1,10 @@
 import { defineCustomElement as defineModal } from '@ionic/core/components/ion-modal';
+import { type Components } from '@ionic/core/components';
 
+export type IonModal = Components.IonModal;
 export const IonModal = {
   register() {
     defineModal();
   },
 };
+
